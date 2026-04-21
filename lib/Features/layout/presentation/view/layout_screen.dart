817db3391d1c9171fe475053c5c0b12676541874
AbdElrhman AbdElfattah <abdelrhmanabdelfattah9866@gmail.com>
2026-04-21 22:31:11 +0200

    modify code

--- lib/Features/layout/presentation/view/layout_screen.dart
+++ lib/Features/layout/presentation/view/layout_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:stellar_shop/Features/cart/presentation/view/cart_screen.dart';
-import 'package:stellar_shop/Features/favorite/presentation/view/favorite_screen.dart';
 import 'package:stellar_shop/Features/home/presentation/view/home_screen.dart';
 import '../../../profile/presentation/view/profile_screen.dart';
 import '../view_manager/nav_bar_cubit.dart';
@@ -13,8 +11,6 @@ class LayoutScreen extends StatelessWidget {
   static const String routeName ="LayoutScreen";
   final List<Widget> tabs=[
     const HomeScreen(),
-    const FavoriteScreen(),
-    const CartScreen(),
     const ProfileScreen(),
   ];
 
@@ -51,16 +47,13 @@ class LayoutScreen extends StatelessWidget {
                   haptic: true,
                   tabBorderRadius: 15,
                   duration: const Duration(milliseconds: 250),
-                  gap: 8,
+                  gap: 60,
                   color: Colors.grey[400],
                   iconSize: 27,
                   activeColor: Colors.white,
                   tabBackgroundColor: const Color(0xFFFF6B00),
-                  padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
-                  tabs: const [
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),                  tabs: const [
                     GButton(icon: Icons.home, text: 'Home',),
-                    GButton(icon: Icons.favorite, text: 'Favorite',),
-                    GButton(icon: Icons.shopping_cart_outlined, text: 'Cart',),
                     GButton(icon: Icons.person, text: 'Profile',),
                   ],
                 ),
